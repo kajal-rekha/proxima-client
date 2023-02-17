1202c0b116799c0ce42f8fc0c46337640c650485
kajal-2011 <kajalrekha888@gmail.com>
2023-02-17 22:47:07 +0600

    context api setup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-
+import { ProjectContextProvider } from "./context/ProjectContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
